ab9f37e0f3ef815fa80b6498ca2a17d9c0f1088e
BabenkovTrofim <babenkovt@bk.ru>
2025-09-07 23:05:12 +0500

    Task1

---
+++ Tyuiu.BabenkovTO.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World");
+        string name = "Трофим";
+        int a = 18;
+        Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + a + " лет");
+
+    }
+}
