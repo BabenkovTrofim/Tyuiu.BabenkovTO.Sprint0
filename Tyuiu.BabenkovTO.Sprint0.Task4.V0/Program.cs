70e4964417c0c11b1968b47f0d0060db68e89bfa
BabenkovTrofim <babenkovt@bk.ru>
2025-09-10 20:27:13 +0500

    Task4

---
+++ Tyuiu.BabenkovTO.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.BabenkovTO.Sprint0.Task4.V0.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.Addiction(1, 5));
+        Console.WriteLine(DataService.Subtraction(15, 5));
+        Console.WriteLine(DataService.Multiplication(10, 10));
+        Console.WriteLine(DataService.Division(5, 5));
+    }
+}
